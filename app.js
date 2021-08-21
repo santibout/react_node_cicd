6afe29bd4d1004674d4279cb5075b1e8fc194a0a
samuel santibout <z.e.r.o-1@samuels-MBP.home>
2021-08-21 14:02:40 -0700

    removed express static middleware

--- app.js
+++ app.js
@@ -4,24 +4,24 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
-// const whitelist = ['http://localhost:3000', 'https://cicd-tutorial-dude.herokuapp.com/'];
-// const corsOptions = {
-//     origin: (origin, cb) => {
-//         console.log('** Origin of request ** ' + origin);
-//         if (whitelist.indexOf(origin) !== -1 || !origin) {
-//             console.log('Origin Acceptable');
-//             cb(null, true);
-//         } else {
-//             console.log('Origin Rejected');
-//             cb(new Error('Not allowed by cors'));
-//         }
-//     }
-// };
+const whitelist = ['http://localhost:3000', 'https://cicd-tutorial-dude.herokuapp.com/'];
+const corsOptions = {
+    origin: (origin, cb) => {
+        console.log('** Origin of request ** ' + origin);
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log('Origin Acceptable');
+            cb(null, true);
+        } else {
+            console.log('Origin Rejected');
+            cb(new Error('Not allowed by cors'));
+        }
+    }
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'client/')));
+    // app.use(express.static(path.join(__dirname, 'client/')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
