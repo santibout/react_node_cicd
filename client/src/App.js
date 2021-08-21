fbc46f41d5b14d9e135d1115c65bbdb276e5f7a9
samuel santibout <z.e.r.o-1@samuels-MBP.home>
2021-08-21 15:26:17 -0700

    modified app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import './App.css';
+
 function App() {
   const [username, setUsername] = useState();
 
   useEffect(() => {
-    // getNames();
-    console.log('react app started');
+    getNames();
   }, []);
 
   const getNames = async () => {
@@ -16,8 +17,7 @@ function App() {
 
   return (
     <div className='app'>
-      {/* <h1>username: {username}</h1> */}
-      <h1>Hi, I'm Sam</h1>
+      <h1>username: {username}</h1>
     </div>
   );
 }
