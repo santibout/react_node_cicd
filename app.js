523de53a9df1b66a3f7a51e07cbe8918d59ad3a7
samuel santibout <z.e.r.o-1@samuels-MBP.home>
2021-08-21 13:41:49 -0700

    changed package.json git push origin master

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
